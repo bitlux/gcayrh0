2795cceb274a5c983882111ff1e6ae54a94af378
Adam Bender <abender@gmail.com>
2024-10-02 22:29:08 -0700

    Initial commit of puzzle-generating code.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bitlux/gcayrh0
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func extractWords(filename string) ([][]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("could not open file: %v", err)
+	}
+	defer file.Close()
+
+	words := [][]string{}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		words = append(words, strings.Fields(strings.ToLower(scanner.Text())))
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error scanning: %v", err)
+	}
+	return words, nil
+}
+
+func hash(data string) string {
+	sum := sha256.Sum256([]byte(data))
+	return fmt.Sprintf("%x", sum)
+}
+
+func main() {
+	filename := os.Args[1]
+	words, err := extractWords(filename)
+	if err != nil {
+		fmt.Printf("error reading file %q: %v\n", filename, err)
+		os.Exit(1)
+	}
+
+	for _, line := range words {
+		for _, word := range line {
+			fmt.Printf("%s ", hash(word))
+		}
+		fmt.Println()
+	}
+}
